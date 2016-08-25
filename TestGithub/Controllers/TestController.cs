5b591c47a211f737f54292f7504e358489a9233d
amisey <amirseydilo@gmail.com>
2016-08-25 14:49:08 +0430

    33

--- TestGithub/Controllers/TestController.cs
+++ TestGithub/Controllers/TestController.cs
@@ -13,5 +13,10 @@ namespace TestGithub.Controllers
         {
             return View();
         }
+
+        public ActionResult Indexx()
+        {
+            return View();
+        }
     }
 }
